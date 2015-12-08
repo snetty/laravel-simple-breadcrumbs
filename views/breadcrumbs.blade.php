f7cfea9d152c8f898b14affcea2cc35e0ba52371
Simon Stevens <simon@3dpixel.uk>
2015-12-08 08:47:12 +0000

    fixes bug caused by rogue / in href

--- views/breadcrumbs.blade.php
+++ views/breadcrumbs.blade.php
@@ -2,7 +2,7 @@
 	<ol class="breadcrumb">
 		@foreach ($breadcrumbs as $breadcrumb)
 			@if ($breadcrumb->url && ($breadcrumbs->last() != $breadcrumb))
-				<li><a href="/{{{ url($breadcrumb->url) }}}">{{{ $breadcrumb->title }}}</a></li>
+				<li><a href="{{{ url($breadcrumb->url) }}}">{{{ $breadcrumb->title }}}</a></li>
 			@else
 				<li class="active">{{{ $breadcrumb->title }}}</li>
 			@endif
